5156b3b0f632cfe79fb89f55ba8e99da09bee89b
aeros96 <44295833+aeros96@users.noreply.github.com>
2020-05-22 10:10:43 -0400

    newlinks

--- plugin.video.smartIPTV/_Edit.py
+++ plugin.video.smartIPTV/_Edit.py
@@ -2,5 +2,5 @@ import xbmcaddon
 import base64
 
 Decode = base64.decodestring
-MainBase = base64.b64decode ('aHR0cHM6Ly9hZXJvczk2LjAwMHdlYmhvc3RhcHAuY29tL2J1aWxkL3VwbG9hZHovc21hcnRuZW1haW4ueG1s')
+MainBase = base64.b64decode ('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2Flcm9zOTYvc21hcnRpcHR2L21hc3Rlci9zbWFydG5lbWFpbi54bWw=')
 addon = xbmcaddon.Addon('plugin.video.smartIPTV')
